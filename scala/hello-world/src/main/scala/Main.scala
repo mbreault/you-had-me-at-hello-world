5b037db8af4b779dc8f086ad96e3dc618eaa809b
mbreault <mbreault@gmail.com>
2018-07-29 13:24:28 -0400

    init scala version

---
+++ scala/hello-world/src/main/scala/Main.scala
@@ -0,0 +1,31 @@
+/*
+1. HTTP GET example.com
+2. Regex title out of HTML
+3. Reverse string
+4. Write to a text file.
+*/
+
+import scalaj.http._
+import scala.util.matching.Regex
+import java.io._
+
+object Main extends App {
+  
+  // 1. HTTP GET example.com
+  val response: HttpResponse[String] = Http("http://example.com").asString
+  val body = response.body
+
+  body.replace('\r', ' ')
+
+  // 2. Regex title out of HTML
+  val regexTitlePattern = "<title>(.*)</title>".r
+  val someTitle = for (m <- regexTitlePattern.findFirstMatchIn(body)) yield m.group(1)
+  val title = someTitle.get
+  println(title)
+  val reverseTitle = title.reverse
+  println(reverseTitle)
+
+  // 3. Reverse string
+  // Apparently scala doesn't have file i/o (that seems odd).  Everything online says to use Java
+  new PrintWriter("output.txt") { write(reverseTitle); close }
+}
