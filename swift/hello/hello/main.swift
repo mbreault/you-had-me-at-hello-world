5abac987010e41048d6fefaaed4274702b6764eb
mbreault <mbreault@gmail.com>
2018-12-09 18:43:05 -0500

    initial commit

---
+++ swift/hello/hello/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  hello
+//
+//  Created by Michael Breault on 12/9/18.
+//  Copyright © 2018 Embersilk. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
